6b68431dc7281528b225ef9dabaa1c793b6465e5
Shashank Garewal <42548072+ShashankIITG@users.noreply.github.com>
2019-03-04 09:23:35 +0530

    Create softether.sh

---
+++ softether.sh
@@ -0,0 +1,48 @@
+sudo su
+apt install checkinstall build-essential
+wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.29-9680-rtm/softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-x64-64bit.tar.gz
+tar -xzf softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-x64-64bit.tar.gz
+cd vpnserver
+make
+cd ..
+mv vpnserver/ /usr/local
+chmod 600 * /usr/local/vpnserver/
+chmod 700 /usr/local/vpnserver/vpncmd && chmod 700 /usr/local/vpnserver/vpnserver
+
+echo '#!/bin/sh
+# description: SoftEther VPN Server
+### BEGIN INIT INFO
+# Provides:          vpnserver
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: softether vpnserver
+# Description:       softether vpnserver daemon
+### END INIT INFO
+DAEMON=/usr/local/vpnserver/vpnserver
+LOCK=/var/lock/subsys/vpnserver
+test -x $DAEMON || exit 0
+case "$1" in
+start)
+$DAEMON start
+touch $LOCK
+;;
+stop)
+$DAEMON stop
+rm $LOCK
+;;
+restart)
+$DAEMON stop
+sleep 3
+$DAEMON start
+;;
+*)
+echo "Usage: $0 {start|stop|restart}"
+exit 1
+esac
+exit 0' > /etc/init.d/vpnserver
+
+chmod 755 /etc/init.d/vpnserver
+update-rc.d vpnserver defaults
+/etc/init.d/vpnserver start
